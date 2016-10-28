7524fd68c6907c1f6c76eba837ce1332a86cb302
Gabriel Nikol <gabriel-martin.nikol@stud.uni-bamberg.de>
2016-10-28 19:26:34 +0200

    created init_mosq_db.s to create database with schema in mysql-mosq container

---
+++ init_mosq_db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script creates the table layout of the database given in mos_db.conf
+
+IPAddress=$(sudo docker inspect mysql-mosq | grep -m3 IPAddress | tail -n1 | cut -d '"' -f 4)
+mysql -uroot -pmypassword -h${IPAddress} -P3306 < mosq_db.conf
+
